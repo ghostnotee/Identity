99cf2f1a4e939e8c8e34f4fdaebb554f350272ea
Selçuk Bilgen <selcukbilgen@hotmail.com>
2022-05-10 12:39:48 +0300

    listing users in admin index page of admin  controller

---
+++ Identity/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
