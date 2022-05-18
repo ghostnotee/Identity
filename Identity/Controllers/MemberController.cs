d150c7b201b118ec58cb541dfb51479fbd6460f7
Selçuk Bilgen <selcukbilgen@hotmail.com>
2022-05-18 10:51:59 +0300

    designing member main page

--- Identity/Controllers/MemberController.cs
+++ Identity/Controllers/MemberController.cs
@@ -1,4 +1,8 @@
+using Identity.Models;
+using Identity.Models.ViewModels;
+using Mapster;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers;
@@ -6,9 +10,23 @@ namespace Identity.Controllers;
 [Authorize]
 public class MemberController : Controller
 {
-    // GET
+    private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    
+    public MemberController(ILogger<HomeController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+    {
+        _logger = logger;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+        UserViewModel userViewModel = user.Adapt<UserViewModel>();
+        
+        
+        return View(userViewModel);
     }
 }
