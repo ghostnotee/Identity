27eb366a41ea5821d2c6b7ee8b3c6c667c84f090
Selçuk Bilgen <selcukbilgen@hotmail.com>
2022-05-26 10:18:42 +0300

    assigning roles to members

--- Identity/Controllers/AdminController.cs
+++ Identity/Controllers/AdminController.cs
@@ -104,5 +104,53 @@ namespace Identity.Controllers
 
             return View(roleViewModel);
         }
+
+        public IActionResult RoleAssign(string id)
+        {
+            TempData["userId"] = id;
+
+            var user = _userManager.FindByIdAsync(id).Result;
+            ViewBag.userName = user.UserName;
+
+            var roles = _roleManager.Roles;
+            var userRoles = _userManager.GetRolesAsync(user).Result;
+
+            var listOfRoleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var role in roles)
+            {
+                var roleAssignViewModel = new RoleAssignViewModel
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name
+                };
+
+                roleAssignViewModel.Exist = userRoles.Contains(role.Name);
+
+                listOfRoleAssignViewModels.Add(roleAssignViewModel);
+            }
+
+            return View(listOfRoleAssignViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleAssign(List<RoleAssignViewModel> roleAssignViewModels)
+        {
+            var user = _userManager.FindByIdAsync(TempData["userId"].ToString()).Result;
+
+
+            foreach (var model in roleAssignViewModels)
+            {
+                if (model.Exist)
+                {
+                    _userManager.AddToRoleAsync(user, model.RoleName).Wait();
+                }
+                else
+                {
+                    _userManager.RemoveFromRoleAsync(user, model.RoleName).Wait();
+                }
+            }
+
+            return RedirectToAction("Users");
+        }
     }
 }
