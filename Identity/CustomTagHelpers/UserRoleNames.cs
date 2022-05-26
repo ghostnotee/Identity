dfc9bab9ce9b066f262a87183e28650bc513dfc2
Selçuk Bilgen <selcukbilgen@hotmail.com>
2022-05-26 12:28:55 +0300

    showing assigned roles of members

---
+++ Identity/CustomTagHelpers/UserRoleNames.cs
@@ -0,0 +1,29 @@
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Identity.CustomTagHelpers;
+
+[HtmlTargetElement("td", Attributes = "user-roles")]
+public class UserRoleNames : TagHelper
+{
+    public UserManager<AppUser> UserManager { get; set; }
+    
+    public UserRoleNames(UserManager<AppUser> userManager)
+    {
+        UserManager = userManager;
+    }
+
+    [HtmlAttributeName("user-roles")] public string UserId { get; set; }
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        var user = await UserManager.FindByIdAsync(UserId);
+        var roles = await UserManager.GetRolesAsync(user);
+
+        var html = string.Empty;
+        roles.ToList().ForEach(x => { html += $"<span class='badge badge-alert-success'> {x}  </span>"; });
+
+        output.Content.SetHtmlContent(html);
+    }
+}
